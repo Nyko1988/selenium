022c4dfd79e9aec9e7ab7883b6ebffe685f4f1b6
Mykola_Mykytiuk <nikolai0673710503@gmail.com>
2024-05-11 20:23:37 +0300

    up

--- selen.py
+++ selen.py
@@ -1,15 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# import time
+#
+# driver = webdriver.Chrome()
+# driver.get("https://www.google.com/")
+# input_accept = driver.find_element(By.ID, "L2AGLb")
+# input_accept.click()
+#
+# input_google = driver.find_element(By.XPATH, "//textarea[@name='q']")
+# input_google.send_keys("Iphone")
+# input_google.send_keys(Keys.ENTER)
+# #time.sleep(5)
+# assert "Iphone - Пошук Google" in driver.title
+def add(x, y):
+    return x + y
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
-input_accept = driver.find_element(By.ID, "L2AGLb")
-input_accept.click()
 
-input_google = driver.find_element(By.XPATH, "//textarea[@name='q']")
-input_google.send_keys("Iphone")
-input_google.send_keys(Keys.ENTER)
-#time.sleep(5)
-assert "Iphone - Пошук Google" in driver.title
+# Test case
+def test_add():
+    # Test input
+    result = add(3, 5)
+
+    # Expected output
+    expected_result = 8
+
+    # Assertion
+    assert result == expected_result, f"Expected {expected_result}, but got {result}"
+
+
+# Run the test
+test_add()
+
